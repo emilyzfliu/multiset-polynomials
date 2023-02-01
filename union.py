d2561c73b9c3f0c04f6eba608e090139cbc5b4b6
emilyzfliu <emily.zf.liu@gmail.com>
2023-02-01 14:08:24 -0500

    union code pieces

--- union.py
+++ union.py
@@ -1,2 +1,27 @@
+from utils import *
+import random
+
 '''
+    Multiset "union" check for A, B, C
 '''
+P = 10**9 + 7
+
+def challenge():
+    return random.randint(0, P)
+
+def main():
+    # define A, B, C (known only by prover)
+    A = []
+    B = []
+    C = []
+
+    z_A = RootsRepPoly(A)
+    z_B = RootsRepPoly(B)
+    z_C = RootsRepPoly(C)
+
+    gamma = challenge()
+
+    assert z_A.evaluate(gamma) * z_B.evaluate(gamma) == z_C.evaluate(gamma)
+
+if __name__ == '__main__':
+    main()
